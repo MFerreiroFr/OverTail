cd73fe4d996afefc3dbb3faf053d5ebd8cba2a85
MFerreiroFr <mferreiro@iesjoandaustria.org>
2019-01-21 14:42:57 +0100

    adjusted gap in initial obstacles

--- js/gameScreen.js
+++ js/gameScreen.js
@@ -12,23 +12,23 @@ function GameScreen(name, canvas, changeScreen) {
   this.elements = { 
     player: new Player(this.canvas, this.ctx, this.statusBar.sizeY),
     obstacles: [
-      //new Obstacle(canvas, this.ctx, canvas.width * 2 + 15, 250, 0),
+      new Obstacle(canvas, this.ctx, canvas.width * 2 + 15, 250, 0),
 
-      new Obstacle(canvas, this.ctx, canvas.width * 3 + 15, 175, canvas.height - this.statusBar.sizeY - 175),
-      //new Obstacle(canvas, this.ctx, canvas.width * 3 + 15, 175, 0),
-      new Obstacle(canvas, this.ctx, canvas.width * 3 + 200, 175, canvas.height - this.statusBar.sizeY - 175),
-      new Obstacle(canvas, this.ctx, canvas.width * 3 + 200, 175, 0),
-      new Obstacle(canvas, this.ctx, canvas.width * 3 + 275, 215, canvas.height - this.statusBar.sizeY - 215),
-      new Obstacle(canvas, this.ctx, canvas.width * 3 + 275, 135, 0),
-      new Obstacle(canvas, this.ctx, canvas.width * 3 + 350, 255, canvas.height - this.statusBar.sizeY - 255),
-      new Obstacle(canvas, this.ctx, canvas.width * 3 + 350, 95, 0),
+      new Obstacle(canvas, this.ctx, canvas.width * 3 + 15, 160, canvas.height - this.statusBar.sizeY - 160),
+      new Obstacle(canvas, this.ctx, canvas.width * 3 + 15, 160, 0),
+      new Obstacle(canvas, this.ctx, canvas.width * 3 + 200, 160, canvas.height - this.statusBar.sizeY - 160),
+      new Obstacle(canvas, this.ctx, canvas.width * 3 + 200, 160, 0),
+      new Obstacle(canvas, this.ctx, canvas.width * 3 + 275, 200, canvas.height - this.statusBar.sizeY - 200),
+      new Obstacle(canvas, this.ctx, canvas.width * 3 + 275, 120, 0),
+      new Obstacle(canvas, this.ctx, canvas.width * 3 + 350, 240, canvas.height - this.statusBar.sizeY - 240),
+      new Obstacle(canvas, this.ctx, canvas.width * 3 + 350, 80, 0),
 
-      new Obstacle(canvas, this.ctx, canvas.width * 4 + 200, 255, canvas.height - this.statusBar.sizeY - 255),
-      new Obstacle(canvas, this.ctx, canvas.width * 4 + 200, 95, 0),
-      new Obstacle(canvas, this.ctx, canvas.width * 4 + 275, 215, canvas.height - this.statusBar.sizeY - 215),
-      new Obstacle(canvas, this.ctx, canvas.width * 4 + 275, 135, 0),
-      new Obstacle(canvas, this.ctx, canvas.width * 4 + 350, 175, canvas.height - this.statusBar.sizeY - 175),
-      new Obstacle(canvas, this.ctx, canvas.width * 4 + 350, 175, 0),
+      new Obstacle(canvas, this.ctx, canvas.width * 4 + 200, 240, canvas.height - this.statusBar.sizeY - 240),
+      new Obstacle(canvas, this.ctx, canvas.width * 4 + 200, 80, 0),
+      new Obstacle(canvas, this.ctx, canvas.width * 4 + 275, 200, canvas.height - this.statusBar.sizeY - 200),
+      new Obstacle(canvas, this.ctx, canvas.width * 4 + 275, 120, 0),
+      new Obstacle(canvas, this.ctx, canvas.width * 4 + 350, 160, canvas.height - this.statusBar.sizeY - 160),
+      new Obstacle(canvas, this.ctx, canvas.width * 4 + 350, 160, 0),
     ]
   }
 }
