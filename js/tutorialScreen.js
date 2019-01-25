5073e1b8876e81927ea8113fcfdb58a80ec473cf
MFerreiroFr <mferreiro@iesjoandaustria.org>
2019-01-25 11:23:00 +0100

    clarify tutorial end screen

--- js/tutorialScreen.js
+++ js/tutorialScreen.js
@@ -39,10 +39,12 @@ function TutorialScreen(name, canvas, changeScreen) {
       new TextObstacle(canvas, this.ctx,canvas.width * 9, 0, 50, "Try not to get hit \u2764"),
       new TextObstacle(canvas, this.ctx,canvas.width * 10 + 120, 0, 50, "Are you ready to show your"),
       new TextObstacle(canvas, this.ctx,canvas.width * 10 + 120, 0, 75, "DETERMINATION?"),
-      new TextObstacle(canvas, this.ctx,canvas.width * 11 + 300, 0, 50, "I was born ready \u21e8"),
-      new TextObstacle(canvas, this.ctx,canvas.width * 11 + 300, 0, 300, "I need more time \u21e8"),
-      new EndObstacle(canvas, this.ctx, canvas.width * 12 + 50, 199, 0, "next"),
-      new EndObstacle(canvas, this.ctx, canvas.width * 12 + 50, 199, canvas.height - this.statusBar.sizeY - 197),
+      new TextObstacle(canvas, this.ctx,canvas.width * 11 + 300, 0, 50, "Go up or down"),
+      new TextObstacle(canvas, this.ctx,canvas.width * 11 + 300, 0, 75, "to make a decision"),
+      new TextObstacle(canvas, this.ctx,canvas.width * 12 + 250, 0, 50, "I was born ready \u21e8"),
+      new TextObstacle(canvas, this.ctx,canvas.width * 12 + 250, 0, 300, "I need more time \u21e8"),
+      new EndObstacle(canvas, this.ctx, canvas.width * 13, 199, 0, "next"),
+      new EndObstacle(canvas, this.ctx, canvas.width * 13 + 50, 195, canvas.height - this.statusBar.sizeY - 197),
     ]
   }
   this.levelLength = this.elements.obstacles.length;
